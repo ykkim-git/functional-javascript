4b1b0cbded09abe9756bced48056e9c6b6e13647
ykkim <44081957+ykkim-git@users.noreply.github.com>
2020-11-02 23:58:14 +0900

    commit day1 & 2

---
+++ js/_.js
@@ -0,0 +1,25 @@
+
+function _filter(list, predi) {
+  var new_list = [];
+  _each(list, function(val) {
+    if(predi(val)) {
+      new_list.push(users[i]);
+    }
+  })
+  return new_list;
+}
+
+function _map(list, mapper) {
+  var new_list = [];
+  _each(list, function(val) {
+    new_list.push(mapper(val));
+  });
+  return new_list;
+}
+
+function _each(list, iter) {
+  for (var i=0; i<list.length; i++) {
+    iter(list[i]);
+  }
+  return list;
+}
